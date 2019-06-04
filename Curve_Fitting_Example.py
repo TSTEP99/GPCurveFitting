e69f8d552ea04d90b728f1902b7f2b3dd6a77bd1
TSTEP99 <43427263+TSTEP99@users.noreply.github.com>
2019-06-03 21:47:58 -0500

    Added some comments to provide clarity

--- Curve_Fitting_Example.py
+++ Curve_Fitting_Example.py
@@ -2,21 +2,21 @@ import numpy as np;
 import matplotlib.pyplot as plt
 from GP import *;
 
-x=np.arange(-5,5.1,.1);
-func_set=[add,subtract,divide,multiply];
-term_set=[i for i in range(-5,6)];
-for i in range(-5,6):
+x=np.arange(-5,5.1,.1); # intilizes as array to be numbers from -5 to 5 with increments of .1
+func_set=[add,subtract,divide,multiply]; # function set of add,subtract,divide,multiply from GP.py
+term_set=[i for i in range(-5,6)]; # adds constants from -5 to 5
+for i in range(-5,6): # adds x 5 times so it the term_set is not dominated by constants
 	term_set.append(x);
 
-test=GP_Program(term_set,func_set);
+test=GP_Program(term_set,func_set); # initilizes instance of the GP_program class
 
-y=x*x+x+1;
-root=test.run(500,300,2,y);
+y=x*x+x+1; #y=x^2+x+1
+root=test.run(500,300,2,y); #runs the GP algorithm with a 500 population size for 300 generations and max depth of 2
 print(test.fitness(y,root));
 
 
 			
-plt.plot(x,y,color="green");
-plt.plot(x,test.tree_interpreter(root),color="blue");
+plt.plot(x,y,color="green"); # Plots the correct data with the color green
+plt.plot(x,test.tree_interpreter(root),color="blue"); #plots the apporixamated function with the color blue
 
 plt.show()
